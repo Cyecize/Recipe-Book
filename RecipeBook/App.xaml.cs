b38af30dd1f175e98e249ceb18eafbe7f49819b2
Tsvetelin Yakimov <ceci2205@abv.bg>
2020-07-05 01:19:18 +0300

    Made process of showing form code based.

--- RecipeBook/App.xaml.cs
+++ RecipeBook/App.xaml.cs
@@ -7,5 +7,12 @@ namespace RecipeBook
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
