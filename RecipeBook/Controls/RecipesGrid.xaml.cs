8b40e9022ebe2a51cfa23a5bbf3ca40b8eed91ee
Tsvetelin Yakimov <ceci2205@abv.bg>
2020-07-06 21:52:29 +0300

    Added Recipes Grid.

---
+++ RecipeBook/Controls/RecipesGrid.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace RecipeBook.Controls
+{
+    /// <summary>
+    /// Interaction logic for RecipesGrid.xaml
+    /// </summary>
+    public partial class RecipesGrid : UserControl
+    {
+        public RecipesGrid()
+        {
+            InitializeComponent();
+        }
+    }
+}
