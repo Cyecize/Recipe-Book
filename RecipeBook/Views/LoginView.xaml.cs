622be5e50fb7e3769733d5c78d54873ce3143765
Tsvetelin Yakimov <ceci2205@abv.bg>
2020-07-06 13:00:08 +0300

    Added masking to login and register password fields.

--- RecipeBook/Views/LoginView.xaml.cs
+++ RecipeBook/Views/LoginView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace RecipeBook.Views
 {
@@ -10,6 +11,18 @@ namespace RecipeBook.Views
         public LoginView()
         {
             InitializeComponent();
+        }
+
+        private void PbPassword_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            string text = this.PbPassword.Password;
+            this.TbPassword.Text = text;
+        }
+
+        private void TbPassword_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.PbPassword.Password != this.TbPassword.Text)
+                this.PbPassword.Password = this.TbPassword.Text;
         }
     }
 }
--- RecipeBook/Views/RegisterView.xaml.cs
+++ RecipeBook/Views/RegisterView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace RecipeBook.Views
 {
@@ -10,6 +11,30 @@ namespace RecipeBook.Views
         public RegisterView()
         {
             InitializeComponent();
+        }
+
+        private void PbPasswordConfirm_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            string text = this.PbPasswordConfirm.Password;
+            this.TbPasswordConfirm.Text = text;
+        }
+
+        private void PbPassword_OnKeyUp(object sender, KeyEventArgs e)
+        {
+            string text = this.PbPassword.Password;
+            this.TbPassword.Text = text;
+        }
+
+        private void TbPassword_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.PbPassword.Password != this.TbPassword.Text)
+                this.PbPassword.Password = this.TbPassword.Text;
+        }
+
+        private void TbPasswordConfirm_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.PbPasswordConfirm.Password != this.TbPasswordConfirm.Text)
+                this.PbPasswordConfirm.Password = this.TbPasswordConfirm.Text;
         }
     }
 }
